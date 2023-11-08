dfdbb3f2e0d1371c6f9ff1356ccfb0576af39de3
Denis Patterson <patterd2@users.noreply.github.com>
2023-11-08 09:23:36 +0000

    Updates to CH3 examples

--- CH3_example_2.m
+++ CH3_example_2.m
@@ -8,26 +8,47 @@ numberofrealisations=5;
 n=finaltime/dt;
 X=zeros(numberofrealisations,n);
 Y=zeros(numberofrealisations,n);
+Z=zeros(numberofrealisations,n);
 
 for i=1:numberofrealisations
-    dX = sqrt(2*D*dt)*randn(1,n);   
-    X(i,:)=cumsum(dX(1,:));             
-    dY = sqrt(2*D*dt)*randn(1,n);   
-    Y(i,:) = cumsum(dY(1,:));             
+    dX = sqrt(2*D*dt)*randn(1,n);
+    X(i,:)=cumsum(dX(1,:));
+    dY = sqrt(2*D*dt)*randn(1,n);
+    Y(i,:) = cumsum(dY(1,:));
+    dZ = sqrt(2*D*dt)*randn(1,n);
+    Z(i,:) = cumsum(dZ(1,:));
 end
 
+% need to downsample for readability
 Xplot(:,:)=X(:,5:5:n);
 Yplot(:,:)=Y(:,5:5:n);
+Zplot(:,:)=Z(:,5:5:n);
+
 
 %% Plotting
 figure(1);
 for j = 1:numberofrealisations
-    plot([0,Xplot(j,:)],[0,Yplot(j,:)]);   
+    plot([0,Xplot(j,:)],[0,Yplot(j,:)]);
     hold on;
-    plot([X(j,n)],[Y(j,n)],'ko','Linewidth',5);   
+    plot([X(j,n)],[Y(j,n)],'ko','Linewidth',5);
+    plot(0,0,'ro','Linewidth',5);
 end
 xlabel('X [mm]');
 ylabel('Y [mm]');
+axis tight
+set(gca,'Fontsize',20);
+grid on;
+
+figure(2);
+for j = 1:min(1,numberofrealisations)
+    plot3([0,Xplot(j,:)],[0,Yplot(j,:)],[0,Zplot(j,:)])
+    hold on;
+    plot3([X(j,n)],[Y(j,n)],[Z(j,n)],'ko','Linewidth',5);
+    plot3(0,0,0,'ro','Linewidth',5);
+end
+xlabel('X [mm]');
+ylabel('Y [mm]');
+zlabel('Z [mm]');
 axis tight
 set(gca,'Fontsize',20);
 grid on;
--- CH3_example_4.m
+++ CH3_example_4.m
@@ -4,7 +4,7 @@ k1=0.001;
 k2=0.75;
 k3=165;
 k4=10000;
-k5=200;
+k5=200; % diffusion coefficient
 
 dt=0.01;
 finaltime=800; 
@@ -13,12 +13,15 @@ n=finaltime/dt;
 X=zeros(n+1,1);
 time=0:dt:finaltime;
 
+%X(1) = 500; % change initial condition from zero
 for i=1:n
     X(i+1) = X(i)+dt*(-k1*X(i)*X(i)*X(i)+k2*X(i)*X(i)-k3*X(i)+k4)+k5*sqrt(dt)*randn(1,1);   
 end
 
 [t,z] = ode15s(@(t,z) -k1*z(1)*z(1)*z(1)+k2*z(1)*z(1)-k3*z(1)+k4,[0 finaltime],0);
 
+
+%% Plotting
 figure(1);
 plot(time,X,'b');   
 hold on;
@@ -28,6 +31,6 @@ ylabel('X(t)');
 set(get(gca,'ylabel'),'rotation',0);
 legend('SDE','ODE');
 axis tight
-box on;
+ylim([0 700]);
 set(gca,'Fontsize',20);
 grid on;
